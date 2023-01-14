c8a176d9f114c33dc7950e697c7f6b28afafac75
Juan Miguel Guerrero <juanmiguel@existo.es>
2023-01-14 16:48:20 +0100

    test funuctions

---
+++ functions/test.js
@@ -0,0 +1,25 @@
+const handler = async (event, context) => {
+	
+	// event.path - The request path
+	// event.httpMethod - The request HTTP method
+	// event.headers - The request headers
+	// event.queryStringParameters - The request query parameters
+	// event.body - The request body in JSON format
+	// event.isBase64Encoded - A boolean flag to indicate if the applicable request payload is Base64-encode
+
+	console.log('Test function called...')
+
+
+	return {
+
+		// isBase64Encoded: true|false,
+		// statusCode: httpStatusCode,
+		// headers: { "headerName": "headerValue", ... },
+		// body: "..."
+
+		statusCode: 200,
+		body: JSON.stringify({message: "Test function..."}),
+	}
+}
+
+export { handler }
